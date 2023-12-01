5848f6f72b6cb05fe554b0a24d9eff41c9ba0fe9
Rhea Lau <rhealau2000@gmail.com>
2023-12-01 19:09:41 +0100

    add day 1

---
+++ day_01.py
@@ -0,0 +1,48 @@
+from helper import get_day
+
+data = get_day(1)
+
+q1_sum = 0
+for i in data:
+    num_list = []
+    for j in i:
+        if j.isnumeric():
+            num_list.append(int(j))  
+    q1_sum += (num_list[0]*10 + num_list[-1])
+
+print(q1_sum)
+
+num_dict = {
+    'one':1,
+    'two':2,
+    'three':3,
+    'four':4,
+    'five':5,
+    'six':6,
+    'seven':7,
+    'eight':8,
+    'nine':9
+}
+
+q2_sum = 0
+
+def is_number(line, position):
+    if line[position].isnumeric():
+        return int(line[position])
+    for a,b in num_dict.items():
+        if line[position:position+len(a)] == a:
+            return int(b)   
+
+for line in data:
+    first_num = 0
+    last_num = 0
+    for index in range(len(line)):
+        num = is_number(line, index)
+        if num is not None:
+            if first_num  == 0:
+                first_num = is_number(line, index)
+            last_num = is_number(line, index)
+
+    q2_sum += (first_num*10 + last_num)
+        
+print(q2_sum)
---
+++ helper.py
@@ -0,0 +1,20 @@
+import requests
+import os
+
+def read_cookie():
+    file_path = ".cookie"
+    with open(file_path, 'r') as file:
+        return file.readline()
+    
+def get_day(day):
+    header = {'Cookie':read_cookie()}
+    url = f'https://adventofcode.com/2023/day/{day}/input'
+    file_path = f'data/day_{str(day) if day > 10 else "0"+str(day)}'
+
+    if not os.path.exists(file_path):
+        response = requests.get(url, headers=header)
+        with open(file_path, 'w') as file:
+            file.write(response.text)
+
+    with open(file_path, 'r') as file:
+        return [x.strip() for x in file.readlines()]
